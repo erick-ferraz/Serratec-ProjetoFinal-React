ec0d8e408d9a775b4ce53e4fb21314b8aee55e14
Erick-Ferraz <erickfpc.tech@gmail.com>
2021-11-26 19:57:43 -0300

    Atualização form

--- projeto-final/projeto-final/src/components/Carrinho.jsx
+++ projeto-final/projeto-final/src/components/Carrinho.jsx
@@ -6,29 +6,30 @@ export default (props) => {
 
     React.useEffect(() => { chamadaAPI() }, [])
 
+    const idCliente = 2;
+
     const chamadaAPI = (props) => {
 
         Api.get(`/pedidos`).then((result)=> {
 
             if (result.data?.length > 0) {
-                const pedidoEncontrado = result.data.find((pedido) => pedido.cliente?.id == 1);
+                const pedidoEncontrado = result.data.find((pedido) => pedido.cliente?.id == idCliente);
                 if (pedidoEncontrado) {
                     setItensPedido(pedidoEncontrado.itensPedido);
                 }
             }
         })
-
     };
 
     return (
         <div>
+            <div className="carrinho">
             {ItensPedido.length > 0 && <h3>Itens no carrinho: {ItensPedido.length}</h3>}
             {ItensPedido.length == 0 && <h3>Seu carrinho está vazio</h3>}
-            <div className="carrinho">
                 <p>Produtos: </p>
                 <ul>
                 {ItensPedido.map((itemPedido) => (
-                    <li key={itemPedido.id}>Nome do produto: {itemPedido.produto.nome} - Preço R$ {itemPedido.produto.vlUnitario}</li>
+                    <li key={itemPedido.id}>Nome do produto: {itemPedido.produto.nome} - Preço R$ {itemPedido.produto.vlUnitario} - Quantidade: {itemPedido.quantidade}</li>
                 ))}
                 </ul>
             </div>
--- projeto-final/projeto-final/src/components/PaginaProduto.jsx
+++ projeto-final/projeto-final/src/components/PaginaProduto.jsx
@@ -5,11 +5,14 @@ import { Link } from 'react-router-dom'
 import BotaoMenu from "./BotaoMenu";
 
 export default (props) => {
-    const {id} = useParams();
-//    const { produto } = props;
+    const { id } = useParams();
+    //    const { produto } = props;
     const [produto, setProduto] = React.useState({});
+    const [qtd, setQtd] = React.useState(1);
 
     React.useEffect(() => { buscar() }, [])
+
+    const clienteId = 2;
 
     function buscar() {
         Api.get(`produtos/${id}`).then(exibir);
@@ -19,30 +22,32 @@ export default (props) => {
         setProduto(response.data);
     }
 
-    function adicionarProd () {
+    function adicionarProd(event) {
+        event.preventDefault();
+        alert('Entrou no event')
         function adicionarItemPedido(idLista) {
             Api.post('/pedidoitens/adicionar', {
                 "pedido": {
-                    "id":idLista
+                    "id": idLista
                 },
                 "produto": {
-                    "id":id
+                    "id": id
                 },
-                "quantidade": 1
-            })
-        } 
+                "quantidade": qtd
+            }).then(() => window.location.href="/carrinho")
+        }
 
-        Api.get(`/pedidos`).then((result)=> {
+        Api.get(`/pedidos`).then((result) => {
 
             if (result.data?.length > 0) {
-                const pedidoEncontrado = result.data.find((x) => x.cliente?.id == 1);
+                const pedidoEncontrado = result.data.find((x) => x.cliente?.id == clienteId);
 
                 if (!pedidoEncontrado) {
                     Api.post("/pedidos/adicionar", {
-                        "cliente":{
-                            "id":1
+                        "cliente": {
+                            "id": clienteId
                         }
-                    }).then((newPedido)=> {
+                    }).then((newPedido) => {
                         adicionarItemPedido(newPedido.data.id);
                     });
                 } else {
@@ -52,16 +57,24 @@ export default (props) => {
         });
     }
 
+    const handleChange = (event) => setQtd(event.target.value);
+
     return (
         <div className="pagina-produto">
             <h1>Nome do produto: {produto.nome}</h1>
             <img src="" alt="" />
-            <h3>Preço: R$ {produto.vlUnitario}</h3>
-            <Link to='/carrinho'><BotaoMenu onClick={adicionarProd}>Adicionar ao carrinho</BotaoMenu></Link>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Possimus quas eveniet, at eum id ducimus nisi dicta excepturi hic 
-                aliquid dignissimos nam rerum distinctio dolores 
+            <h3>Preço: R$ {produto.vlUnitario * qtd}</h3>
+            <Link to='/carrinho'></Link>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Possimus quas eveniet, at eum id ducimus nisi dicta excepturi hic
+                aliquid dignissimos nam rerum distinctio dolores
                 nesciunt officia et. Doloremque, doloribus.</p>
+
+            <form onSubmit={adicionarProd}>
+                <label className='label'>Quantidade</label>
+                <input className="input" type="number" value={qtd} onChange={handleChange} min="0"/>
+                <BotaoMenu type="submit">Adicionar ao carrinho</BotaoMenu>
+            </form>
         </div>
     )
 }
